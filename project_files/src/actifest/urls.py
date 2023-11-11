e3659528908874085b0a10bcef6a04ed7a09d036
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2023-11-11 12:28:10 +0100

    started working on the database

--- project_files/src/actifest/urls.py
+++ project_files/src/actifest/urls.py
@@ -2,7 +2,8 @@ from  django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name = 'index-page')
+    path('',views.index,name = 'index-page'),
+    path('login',views.login,name = 'login-page'),
 
 
 ]
