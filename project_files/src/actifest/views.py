b1fb6ecbab9717ca19e74a194a486d4ea441b451
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2023-11-11 11:51:36 +0100

    views setup

--- project_files/src/actifest/views.py
+++ project_files/src/actifest/views.py
@@ -1,2 +1,13 @@
-from django.shortcuts import render
+from django.http import  HttpResponse,HttpResponseRedirect
+from django.shortcuts import render,redirect
+from .models import *
+from django.urls import reverse
 
+def index(request) :
+    return render(request,'index.html')
+def login(request) :
+    return HttpResponse("login page")
+def registerPage(request) :
+    return HttpResponse("register page")
+def profile (request) :
+    return HttpResponse("profile page")
